e2fa6d2e2f702fba142e92881ec05d2b410a4828
James Mann <mann.james0101@gmail.com>
2026-07-19 01:06:49 +0100

    Made campaign select screen look nicer

--- frontend/src/components/CampaignCard.tsx
+++ frontend/src/components/CampaignCard.tsx
@@ -9,21 +9,10 @@ const CampaignCard = (props: any) => {
 
   return (
     <div
-      className="entity-card"
+      className="campaign-card"
       onClick={() => navigate(`/campaigns/${campaignID}/dashboard`)}
     >
       <div className="entity-header">
-        <div className="entity-image">
-          <img
-            src={
-              campaign.image
-                ? `${API_URL}/${campaign.image}`
-                : "https://placehold.co/120x120?text=Placeholder"
-            }
-            alt={campaign.name}
-          />
-        </div>
-
         <div className="entity-title">
           <h2>{campaign.name}</h2>
         </div>
