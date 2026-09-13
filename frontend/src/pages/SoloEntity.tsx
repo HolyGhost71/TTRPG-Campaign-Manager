d0328be2a143bfa868db7931131eadaa561c44cf
James Mann <mann.james0101@gmail.com>
2026-09-13 11:28:10 +0100

    Changed edit button to edit symbol + removed entity card from quests

--- frontend/src/pages/SoloEntity.tsx
+++ frontend/src/pages/SoloEntity.tsx
@@ -55,13 +55,20 @@ export default function SoloEntity() {
 
   return (
     <div className="solo-entity-page">
-      <div className="page-heading">{entity?.name}</div>
+      <div className="session-title">
+        <div className="page-heading">{entity?.name}</div>
+
+        <button
+          className="edit-session-button"
+          onClick={goToEditPage}
+          aria-label="Edit session"
+        >
+          <span className="edit-icon">✎</span>
+        </button>
+      </div>
 
       <div className="entity-card-wrapper">
-        <EntityCard entity={entity ?? {}} />
-        <button onClick={goToEditPage} className="button">
-          Edit
-        </button>
+        {entity?.type != "QUEST" && <EntityCard entity={entity ?? {}} />}
       </div>
 
       <div className="page-subheading">DM Notes</div>
